bdda8dcc6a64f2c77a66145f77f3c3dc5173ae62
Unknown <dannyehb92@gmail.com>
2019-03-31 21:32:55 -0500

    setting device to cpu

--- ddpg_agent.py
+++ ddpg_agent.py
@@ -18,7 +18,8 @@ LR_CRITIC = 1e-3        # learning rate of the critic
 WEIGHT_DECAY = 0        # L2 weight decay
 UPDATE_EVERY = 30
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+# device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+device = torch.device('cpu')
 
 class Agent():
     """Interacts with and learns from the environment."""
